80cb4814ca44db162ad52bb408675e5e124d1e1d
Daniel Lazar <daniel.lazar@cerepro-consulting.com>
2019-01-22 17:20:06 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t daniellazar/multi-client:latest -t daniellazar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t daniellazar/multi-server:latest -t daniellazar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t daniellazar/multi-worker:latest -t daniellazar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push daniellazar/multi-client:latest
+docker push daniellazar/multi-server:latest
+docker push daniellazar/multi-worker:latest
+
+docker push daniellazar/multi-client:$SHA
+docker push daniellazar/multi-server:$SHA
+docker push daniellazar/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=daniellazar/multi-server:$SHA
+kubectl set image deployments/client-deployment client=daniellazar/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=dnaiellazar/multi-worker:$SHA
